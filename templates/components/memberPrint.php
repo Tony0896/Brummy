f048d181a83de5dc326476534e51eb50f6a98a94
TonyKr1 <jacruz@ci-sa.com.mx>
2024-09-12 11:46:43 -0600

    reacomod

---
+++ templates/components/memberPrint.php
@@ -0,0 +1,100 @@
+<div class="print" id="div_MemberPrint" style="display: none; margin: 0">
+    <div class="col-md-3 no_print"></div>
+    <div
+        class="col-md-2 print nobreak"
+        style="
+            text-align: center;
+            border-radius: 10px;
+            padding: 0;
+            background-image: URL('./images/frontbr4.png');
+            background-repeat: no-repeat !important;
+            background-attachment: scroll !important;
+            background-position: center center !important;
+            z-index: auto;
+            background-size: cover !important;
+            -webkit-background-size: cover !important;
+            -moz-background-size: cover !important;
+            -o-background-size: cover !important;
+            margin: 0;
+            padding-bottom: 7px;
+            width: 215px;
+        "
+    >
+        <div style="display: block" class="nobreak">
+            <div style="margin-bottom: 10px">
+                <img
+                    style="max-height: 115px; min-width: 98px; min-height: 115px; max-width: 98px; margin-top: 19px"
+                    src=""
+                    id="fotoCuts1"
+                />
+            </div>
+
+            <div style="display: flex; flex-direction: column">
+                <span class="spanDataCredential" style="text-wrap: nowrap; color: #009071" id="claveCredential1"> &nbsp; </span>
+                <span class="spanDataCredential" style="text-wrap: nowrap; color: #009071" id="nameCredential1"> &nbsp; </span>
+                <span class="spanDataCredential" style="text-wrap: nowrap; color: #009071" id="telefonoCrendential1"> &nbsp; </span>
+                <span class="spanDataCredential" style="text-wrap: nowrap; color: #009071" id="correoCrendential"> &nbsp; </span>
+            </div>
+
+            <div style="display: flex; font-weight: bold; margin-top: 0px">
+                <div style="width: 50%; display: flex"></div>
+                <div style="width: 40%; display: flex; margin-top: 15px">
+                    <img
+                        style="max-height: 70px; margin: auto; margin-left: 25px"
+                        src=""
+                        id="qrCredential1"
+                        height="70"
+                        width="70"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2 no_print"></div>
+    <div
+        class="col-md-2 print nobreak"
+        style="
+            text-align: center;
+            border-radius: 10px;
+            padding: 0;
+            background-image: URL('./images/backtbr.png');
+            background-repeat: no-repeat !important;
+            background-attachment: scroll !important;
+            background-position: center center !important;
+            z-index: auto;
+            background-size: cover !important;
+            -webkit-background-size: cover !important;
+            -moz-background-size: cover !important;
+            -o-background-size: cover !important;
+            margin: 0;
+            width: 215px;
+        "
+    >
+        <div style="display: block" class="nobreak"></div>
+        <div style="margin-bottom: 10px; margin-top: 45px">
+            <div style="display: flex; flex-direction: column">
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap;margin-top: 13px;" id="nameVete"> &nbsp; </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap">Teléfono de atención: </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap" id="telcredentialvete"> &nbsp; </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap">Horarios de atención: </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap" id="horario1credential"> &nbsp; </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap" id="horario2credential"> &nbsp; </span>
+                <span class="spanDataCredential spanDataCredentialx" style="text-wrap: nowrap" id="horario3credential"> &nbsp; </span>
+            </div>
+
+            <div style="margin-top: 20px;display: flex;flex-direction: column;">
+                <span class="spanDataCredential spanDataCredentialxsm" style="text-wrap: nowrap" id="Direccion1credential"> &nbsp; </span>
+                <span class="spanDataCredential spanDataCredentialxsm" style="text-wrap: nowrap" id="Direccion1credentia2"> &nbsp; </span>
+                <br />
+                <br />
+                <span class="spanDataCredential spanDataCredentialxsm" style="font-size: 6px;text-transform: none;font-style: italic;padding: 0px 15px 40px 15px;text-align: justify;"
+                    > Esta membresía NO es una identificación oficial. El uso es exclusivo para la veterina suscrita. Happy pets y Brummy no se
+                    hace responsable por el mal uso de esta membresía. El cliente al aceptar la membresía acepta los terminos y condiciones
+                    publicados en: 
+                    https://brummy.stth.com.mx/terminosycondiciones/</span
+                >
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 no_print"></div>
+</div>
